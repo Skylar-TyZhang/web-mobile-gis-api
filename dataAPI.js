e014ad8d4684692f44bb8b6552374fd37664be35
schatty6 <skylarty6@icloud.com>
2023-02-10 15:36:49 +0000

    Added functionality to support corss origin requests and log in console

--- dataAPI.js
+++ dataAPI.js
@@ -12,5 +12,20 @@ httpServer.listen(4480);
 app.get('/',function(req,res){
     res.send("hello world from the Data API");
 });
+// to support cross origin req
+app.use(function(req,res,next){
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers","X-Requested-With");
+    res.setHeader("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");
+    next();
+    
+});
+
+// to log requests
+app.use(function(req,res,next){
+    let filename=path.basename(req.url);
+    let extension=path.extname(filename);
+    console.log("The file"+filename+"was requested.");
+});
 // always the last bit of the file
 app.use(express.static(__dirname));
