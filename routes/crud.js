e855854662b26a17975bc0889c1435664ef91808
schatty6 <skylarty6@icloud.com>
2023-03-10 11:28:01 +0000

    Created the crud route with two endpoints for test

---
+++ routes/crud.js
@@ -0,0 +1,44 @@
+// configure express
+let express = require('express');
+let pg = require('pg');
+let crud = require('express').Router();
+let fs = require('fs');
+
+//create database connection 
+let os = require('os');
+const userInfo = os.userInfo();
+const user707 = userInfo.username;
+console.log(user707);
+// locate the database login details
+let configtext = "" + fs.readFileSync("/home/" + user707 + "/certs/postGISConnection.js");
+
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+let configarray = configtext.split(",");
+let config = {};
+for (let i = 0; i < configarray.length; i++) {
+    let split = configarray[i].split(':');
+    config[split[0].trim()] = split[1].trim();
+}
+let pool = new pg.Pool(config);
+console.log(config);
+
+// add data parser functionality to the API 
+// so that NodeJS code can read through the individual name/value pairs
+const bodyParser = require('body-parser');
+crud.use(bodyParser.urlencoded({ extended: true }));
+// add a route '/testCRUD' with 2 endpoints
+// one for get
+// one for post
+// test endpoint for GET requests (can be called from a browser URL or AJAX)
+crud.get('/testCRUD', function (req, res) {
+    res.json({ message: req.originalUrl + " " + "GET REQUEST" });
+});
+
+// test endpoint for POST requests - can only be called from AJAX
+crud.post('/testCRUD', function (req, res) {
+    res.json({ message: req.body });
+});
+
+
+// end of the file
+module.exports= crud;
