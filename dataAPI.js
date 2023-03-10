cfdb9a5e7576ac55c3a2fd1482b10c70454384b5
schatty6 <skylarty6@icloud.com>
2023-03-10 11:24:09 +0000

    added a functionality to set up CORs with in dataAPI.js

--- dataAPI.js
+++ dataAPI.js
@@ -22,7 +22,13 @@ app.use(function(req,res,next){
     next();
     
 });
-
+// set up cors
+// adding CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+   });
 // log requests so that it is easier to debug
 app.use(function(req,res,next){
     let filename=path.basename(req.url);
