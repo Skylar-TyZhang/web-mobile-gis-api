07075459e8c3bf6e32eb5cc76e4e9d2b665d129d
schatty6 <skylarty6@icloud.com>
2023-04-11 18:05:56 +0100

    Added SQL query string to post data to the database. The parameters form part of the body to prevent SQL injection.

--- routes/crud.js
+++ routes/crud.js
@@ -26,6 +26,8 @@ console.log(config);
 // so that NodeJS code can read through the individual name/value pairs
 const bodyParser = require('body-parser');
 crud.use(bodyParser.urlencoded({ extended: true }));
+
+//--------------------------Add endpoints----------------------------------------
 // add a route '/testCRUD' with 2 endpoints
 // one for get
 // one for post
@@ -39,28 +41,54 @@ crud.post('/testCRUD', function (req, res) {
     res.json({ message: req.body });
 });
 // Add endpoints to get userID
-crud.get('/userId', function(req, res){
+crud.get('/userId', function (req, res) {
     //res.json({message:req.originalUrl + " " + "GET REQUEST>"});
-    pool.connect(function(err, client,done){
-        if (err){
-            console.log("Not able to get connection "+ err);
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("Not able to get connection " + err);
             res.status(400).send(err);
         }
-    var querystring='select user_id from ucfscde.users where user_name = current_user;'
-    client.query(querystring, function(err, result){
-        done();
-        if (err){
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    })
+        var querystring = 'select user_id from ucfscde.users where user_name = current_user;'
+        client.query(querystring, function (err, result) {
+            done();
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result.rows);
+        })
     })
 });
 // Added endpoint for insert functionality
-crud.post('/insertAssetPoint', function (req, res) {
+crud.post('/insertAssetPoint/', function (req, res) {
     res.json({ message: req.body });
-});
+    // so the parameters form part of the BODY of the request rather than the RESTful API
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("not able to get connection " + err);
+            res.status(400).send(err);
+        }
+        let asset_name = req.body.asset_name;
+        let installation_date = req.body.installation_date;
+
+        var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")',4326)";
+        var querystring = "INSERT into cege0043.asset_information (asset_name,installation_date, location) values ";
+        querystring += "($1,$2,";
+        querystring += geometrystring + ")";
+
+        
+        client.query(querystring, [asset_name,installation_date], function (err, result) {
+                done();
+                if (err) {
+                    console.log(err);
+                    res.status(400).send(err);
+                }
+                res.status(200).send("Form Data " + req.body.name + " has been inserted");
+            });
+
+    });
+})
+
 
 // end of the file
-module.exports= crud;
+module.exports = crud;
